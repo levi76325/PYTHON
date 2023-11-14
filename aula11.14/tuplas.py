2beb087a802b608bcc4dd5afb67fb7892df40e87
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-11-14 16:31:22 -0300

    tuplas python

---
+++ aula11.14/tuplas.py
@@ -0,0 +1,5 @@
+# tuplas - sua principal caracteristica é ser imutavel.
+# parametro - ()
+# caso voce não informa o parametro da lista ou da tupla, o python transforma o conteudo em uma tupla por conta da imutabilidade. 
+tupla = ( 1, 2, 3, 4, 5 )
+
