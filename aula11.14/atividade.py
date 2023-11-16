3cc413171800bcbdc8081ed829e6627af43f77e5
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-11-16 18:22:43 -0300

    atividades  de lista

--- aula11.14/atividade.py
+++ aula11.14/atividade.py
@@ -1,16 +1,33 @@
-lista = []
-lista = [ '40', '80', '10', '70', '30']
-print(lista)
-lista.remove ('10')
-print(lista)
-
 lista_consoante = []
-letra_1 = input("digite uma letra") 
-letra_2 = input("digite uma letra") 
-letra_3 = input("digite uma letra") 
-letra_4 = input("digite uma letra") 
-letra_5 = input("digite uma letra") 
+
+letra_1 = input("digite uma letra: ") 
 
 if letra_1 != 'a' and letra_1 != "e" and letra_1 != 'i' and letra_1 != 'o' and letra_1 != 'u':
     print("é uma consoante!")
     lista_consoante.append(letra_1)
+
+letra_2 = input("digite uma letra: ") 
+
+if letra_2 != 'a' and letra_2 != "e" and letra_2 != 'i' and letra_2 != 'o' and letra_2 != 'u':
+    print("é uma consoante!")
+    lista_consoante.append(letra_2)
+
+letra_3 = input("digite uma letra: ") 
+
+if letra_3 != 'a' and letra_3 != "e" and letra_3 != 'i' and letra_3 != 'o' and letra_3 != 'u':
+    print("é uma consoante!")
+    lista_consoante.append(letra_3)
+
+letra_4 = input("digite uma letra: ") 
+
+if letra_4 != 'a' and letra_4 != "e" and letra_4 != 'i' and letra_4 != 'o' and letra_4 != 'u':
+    print("é uma consoante!")
+    lista_consoante.append(letra_4)
+
+letra_5 = input("digite uma letra: ") 
+
+if letra_5 != 'a' and letra_5 != "e" and letra_5 != 'i' and letra_5 != 'o' and letra_5 != 'u':
+    print("é uma consoante!")
+    lista_consoante.append(letra_5)
+
+print(lista_consoante)
