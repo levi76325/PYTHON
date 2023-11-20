19206c02b7b7d9ff771bf1d7e7f5e9b3494d9e28
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-11-20 16:13:34 -0300

    atividade do dia 20/11

---
+++ aula11.20/atividade_while.py
@@ -0,0 +1,19 @@
+'''
+while True:
+    nota = int(input('Informe a nota!: '))
+    if nota < 0 and nota > 10:
+        print('Esta nota é invalida!.')
+    nota = nota + 1
+'''
+'''
+while True:
+    nome = input("informe o nome de usuario: ")
+    senha = input("informe a senha: ")
+    if nome == senha:
+        print("ERRO, informe os dados novamente")
+    else:
+        print("Voce concluiu o login!!!")
+        break
+
+'''
+    
